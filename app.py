79b7e28a9800457bc16c9f298687926384e55c7e
VasKoz <kozachenkov666@gmail.com>
2020-09-08 12:51:47 +0300

    1 commit

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def post_update(id):
             db.session.commit()
             return redirect('/posts')
         except:
-            return "При редактировasdaании статьи произошла ошибка"
+            return "При редактировasdaаxcvczнии статьи произошла ошибка"
     else:
 
         return render_template("post_update.html", article=article)
